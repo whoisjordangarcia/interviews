e790ca9f833b8c8964e05e313d941387a1dcf942
Jordan Garcia <arickho@gmail.com>
2024-02-17 18:43:12 -0500

    feat: topkfreq medium

--- leetcode/arrayandhash/topKFrequentElements.ts
+++ leetcode/arrayandhash/topKFrequentElements.ts
@@ -7,14 +7,32 @@ Input: nums = [1,1,1,2,2,3], k = 2
 Output: [1,2]
 
 // HashMap
+// Scrap
 {
     [1]: 3
     [2]: 2
     [3]: 1
 }
 
+// O(n) -- linear time
+// Bucket sort --
+// Scrap
+[0 , 1 , 2 , 3 , 4 , 5 , 6]
+[0, 3 , 2,  1, 0, 0, 0] = nums.length
 
+// O(n) -- linear time
+// Bucket sort - most frequent
 
+{
+    [1]: 3
+    [2]: 2
+    [3]: 1
+}
+// [0 , 1 ,  2 ,   3 , 4 , 5 , 6]
+[0, [3] ,[2],  [1], 0, 0,  0] = nums.length
+[1,2] = output
+
+// Time = O(n) + (n)
 
 Example 2:
 Input: nums = [1], k = 1
@@ -31,8 +49,49 @@ Follow up: Your algorithm's time complexity must be better than O(n log n), wher
 */
 
 function topKFrequent(nums: number[], k: number): number[] {
-    
+    const count = {}
+    const freq = []
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i]
+        count[num] = 1 + (count[num] || 0)
+    }
+    // O(n)
+    // {
+    //     [1]: value=3
+    //     [2]: value=2
+    //     [3]: value=1
+    // }
+    console.log(count)
+    //O(n)
+    for (const [n, c] of Object.entries(count)) {
+        if (!freq[c as number]) {
+            freq[c as number] = []
+        }
+        freq[c as number].push(n)
+    }
+    console.log('freq', freq)
+
+    const ans = []
+    //O(n)
+    for (let j = freq.length - 1; j >= 0; j--) {
+        if (!freq[j]) {
+            continue
+        }
+        for(const num of freq[j]) {
+            ans.push(num)
+            console.log(ans)
+            console.log(ans.length, k)
+            if (ans.length == k) {
+                return ans
+            }
+        }
+    }
+    return ans
 };
 
+// Time complexity O(n)
+//  Space count=O(n) + freq=O(n) + ans=O(n)
+// console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2))
+console.log(topKFrequent([1, 2], 2))
 
-console.log(topKFrequent([1,1,1,2,2,3], 2))
+// console.log(topKFrequent([1,1,1,2,2,3], 2))
